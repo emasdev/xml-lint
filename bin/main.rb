645dce866dbb040cc204b9b855ee4b639d57d4e1
Emas <emas.dev@gmail.com>
2020-07-21 09:10:19 -0500

    Fix method next_tag

--- bin/main.rb
+++ bin/main.rb
@@ -3,9 +3,21 @@ include XMLlint
 
 xml = '
 <test>
-  Something inside
-  <stuff1></stuff1>
-  <test></test>
-</test'
+  <test>gfdgfdgdf</test>
+  <test>
+    <test>
+      <test3>      </test3>
+    </test>
+  </test>
+</test>'
 
-xml_lint(xml)
+xml2 = '
+<test>
+  <test>gfd<tesdt> </tes><ghdfgfdgs</test>
+  <test2>
+    <test3>
+    </test3>
+  </test2>
+</test>'
+
+xml2.xml_lint
