b398d4ae519b5398dac6922e782570695da24470
Emas <emas.dev@gmail.com>
2020-07-28 18:32:50 -0500

    Add multiple files selecting through arguments when running the program

--- bin/main.rb
+++ bin/main.rb
@@ -5,19 +5,9 @@ require_relative '../lib/xml_lint.rb'
 include XMLlint
 # rubocop:enable Style/MixinUsage
 
-# rubocop:disable Lint/UselessAssignment
-xml_test1 = '
-<?xml version="1.0" encoding="UTF-8"?>
-<test>
-  <test>gffdgdf</test>
-  <test>
-    <test>
-      <test3>      </test3>
-    </test>
-  </test>
-</test>'
-# rubocop:enable Lint/UselessAssignment
-
-xml_file = File.open('./docs/test5.xml')
-xml = xml_file.read
-xml.xml_lint
+ARGV.each do |file_name|
+  xml_file = File.open("./docs/#{file_name}")
+  xml = xml_file.read
+  puts "\n=== linter #{file_name} ==="
+  xml.xml_lint
+end
